39df730734f04223697706c6491b7acd4e94bcce
DaniloBaptista <danilo.neves21@outlook.com>
2017-02-25 18:42:13 -0300

    App
    App

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "HOI TUDO BOM?!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+<?php include("cabecalho.php")?>
+                <h1>Bem vindo!!</h1>
+                <?php
+                $total = 10 + 20 *2;
+                echo $total;
+                    ?>
+<?php include("rodape.php")?>
