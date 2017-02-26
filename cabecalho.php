165744a6d1615da44ba73cbf154694d549d4e563
DaniloBaptista <danilo.neves21@outlook.com>
2017-02-25 21:33:40 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title> Minha Loja </title>
+        <title> Calculadora </title>
         <link href="css/bootstrap.css" rel="stylesheet">
 		<link href="css/loja.css" rel="stylesheet">
     </head>
