7fa27a4d3840a50f6f3fb280039c8158a7d81a02
DaniloBaptista <danilo.neves21@outlook.com>
2017-02-25 21:53:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
    <meta charset = "UTF-8">
 </head>
 <body>
-   <h1> Calculadora simples teste.</h1>
+   <h1> Calculadora simples em php no bluemix.</h1>
    <form action="" method="post" >
       Primeiro Numero: <input name="num1" type="text"><br>
       Segundo numero: <input name="num2" type="text"><br>
