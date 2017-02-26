d6a5ad3f250cafb646dac058247f22805feacfd1
DaniloBaptista <danilo.neves21@outlook.com>
2017-02-25 21:29:05 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -12,13 +12,6 @@
                 <div class="navbar-hearder">
                 	<a class = "navbar-brand" href = "index.php">Minha Loja</a>
                 </div>
-                <div>	
-                    <ul class="nav navbar-nav">
-                        <li><a href="produto-formulario.php">adicionar produto </a></li>
-                        <li><a href="produto-lista.php">listar produto </a></li>
-                        <li><a href="sobre.php"> sobre</a></li>
-                    </ul>    
-                </div>
             </div>    
         </div>    
         <div class="container">    
